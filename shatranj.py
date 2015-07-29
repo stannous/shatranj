f02b18f8b9bcaa3fd77e924bae7ffdeb3a640681
stannous <stannous@gmail.com>
2015-07-29 17:53:59 -0400

    Added a test for pawn promotion

--- shatranj.py
+++ shatranj.py
@@ -4235,6 +4235,23 @@ def test_checkmated ():
         print "    10.2 draw test: FAILED"
         tests_failed += 1
 
+def test_promotion ():
+    global tests_passed, tests_failed, test_number
+    SEARCH_DEPTH = 5
+    wtm = 1
+    fen = "3r3K/6P1/8/8/8/8/8/3k4"
+    p = Position(fen)
+    print "11. test: promotion"
+    move_list = p.generate_moves(wtm=1)
+    moves,san_moves = p.get_move_list(move_list)
+    san = san_moves.values()
+    print("    promotion moves are %s"%san)
+    if ('Kh7' in san and 'g8=' in san):
+        print "    11.1 promotion moves: PASSED"
+        tests_passed += 1
+    else:
+        print "    11.1 promotion moves: FAILED"
+        tests_failed += 1
 
 def test_icga ():
     """
@@ -4326,6 +4343,7 @@ def test ():
     test_castling()
     test_check()
     test_checkmated()
+    test_promotion()
     print "==========================================="
     print "total tests PASSED=%s  FAILED=%s" % (tests_passed,tests_failed)
     sys.exit()
